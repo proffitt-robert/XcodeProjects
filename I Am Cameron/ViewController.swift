5b9d7f8d30a1c30e4d520c1100caca8ca4d51419
Robert Proffitt <robert.proffitt@outlook.com>
2023-01-26 17:43:57 -0500

    Initial Commit

---
+++ I Am Cameron/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Cameron
+//
+//  Created by Robert Proffitt on 1/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
